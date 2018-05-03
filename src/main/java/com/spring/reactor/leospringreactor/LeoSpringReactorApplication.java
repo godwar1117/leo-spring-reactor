ed4b863cfc9ad416a2a1a843c9c3f6384d668491
leonid.miao <miaolin1117@126.com>
2018-05-03 15:28:25 +0800

    Initial commit

---
+++ src/main/java/com/spring/reactor/leospringreactor/LeoSpringReactorApplication.java
@@ -0,0 +1,12 @@
+package com.spring.reactor.leospringreactor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeoSpringReactorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LeoSpringReactorApplication.class, args);
+	}
+}
